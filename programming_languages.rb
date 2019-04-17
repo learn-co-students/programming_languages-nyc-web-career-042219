b91e42245fc63c363c4d72e0d7e4494e843f6ea5
Won Kim <wonsae.kim@gmail.com>
2019-04-17 04:36:04 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  style_arr = []
+  languages.each do |languages, style|
+    style.each do |lang, type|
+      type.each do |k, v|
+        if new_hash[lang] == nil
+          new_hash[lang] = {k => v}
+        end
+        
+        if new_hash[lang][:style] == nil
+          new_hash[lang][:style] = [languages]
+        else
+          new_hash[lang][:style] << languages
+        end
+      end
+    end
+    
+  end
+  new_hash
 end
