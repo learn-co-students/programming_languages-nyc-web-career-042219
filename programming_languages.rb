9dc477db68e4611641aa7648a0bd77359b0449b1
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-21 16:45:05 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,24 @@
-
 def reformat_languages(languages)
-new_hash = {}
-languages.each do |style, language| 
-  
-  
+new_languages = {}
+languages.each do |style, language|
+  language.each do |name, attributes|
+  new_languages[name] ||= attributes
+  new_languages[name][:style]=[]
+  new_languages[name][:style] << style
 end
+end
+new_languages
+end
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+  }
+}
+
+reformat_languages(languages)
